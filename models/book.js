3bd312bdfb75d46b35fa395e2452765866a62db4
Penny <radiogaga66@gmail.com>
2019-05-10 18:14:29 -0500

    set up models

---
+++ models/book.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * Return undefined for a value that is null or an empty string.
+ * This prevents mongoose from sending an empty string property to the
+ * database.
+ *
+ * @param {*} v The value to test.
+ */
+function removeEmpty(v) {
+  if (v == null || v === '') {
+    return undefined;
+  }
+
+  return v;
+}
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    set: removeEmpty,
+  },
+  authors: {
+    type: [String],
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  googleId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+module.exports = mongoose.model('Book', bookSchema);
